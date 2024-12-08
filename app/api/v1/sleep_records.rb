af3a17e3773acef4be0ff72c8c7dbeb67de1acc1
Fikry Maulana <fikry.maulana@gmail.com>
2024-12-09 04:42:34 +0700

    Clock out API in the Sleep record

--- app/api/v1/sleep_records.rb
+++ app/api/v1/sleep_records.rb
@@ -9,6 +9,28 @@ module V1
         record = current_user.sleep_records.create!(clock_in: Time.now)
         { id: record.id, clock_in: record.clock_in }
       end
+
+      desc 'Clock out a sleep record'
+      params do
+        optional :sleep_record_id, type: Integer
+      end
+      put :clock_out do
+        # TODO: Do we need to check whether the previous clock in time is more than 24 hours?
+        record = current_user.sleep_records
+                  .where(clock_out: nil)
+        
+        record = record.where(id: params[:sleep_record_id]) if params[:sleep_record_id].present?
+        
+        record = record.order(:created_at).last
+
+        if record.nil?
+          error!('No active sleep record found to clock out', 404)
+        else
+          record.update!(clock_out: Time.now)
+
+          { id: record.id, clock_in: record.clock_in, clock_out: record.clock_out }
+        end
+      end
     end
   end
 end
--- spec/api/v1/sleep_records_spec.rb
+++ spec/api/v1/sleep_records_spec.rb
@@ -12,5 +12,26 @@ RSpec.describe V1::SleepRecords, type: :request do
       res = JSON.parse(response.body)
       expect(res['id']).to be_present
     end
+
+    it 'returns an error when trying to clock out without a clock in record' do
+      put '/api/v1/sleep_records/clock_out', headers: headers
+
+      expect(response).to have_http_status(:not_found)
+      body = JSON.parse(response.body)
+      expect(body['error']).to eq('No active sleep record found to clock out')
+    end
+  end
+
+  context 'when an active clocked-in record exists' do
+    it 'returns success when clocking out' do
+      post '/api/v1/sleep_records', headers: headers
+
+      put "/api/v1/sleep_records/clock_out", headers: headers
+
+      expect(response).to have_http_status(:ok)
+      res = JSON.parse(response.body)
+      expect(res['id']).to be_present
+      expect(res['clock_out']).to be_present
+    end
   end
 end
