ab2c263021c848335ae679601e651ab9cd163779
Fikry Maulana <fikry.maulana@gmail.com>
2024-12-08 11:14:23 +0700

    Create seeder for User table for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+
+# Generate test data for development
+# Execute only in development environment
+if Rails.env.development?
+  # Generate random users
+  5.times do
+    User.create(name: Faker::Name.name, created_at: Time.now, updated_at: Time.now)
+  end
+end
