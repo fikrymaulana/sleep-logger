8809c5800bebaa43dec5088cebc4774733239761
Fikry Maulana <fikry.maulana@gmail.com>
2024-12-08 21:43:26 +0700

    Add API key column for user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_08_045100) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_08_144151) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_08_045100) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at", precision: nil
+    t.string "api_key"
+    t.index ["api_key"], name: "index_users_on_api_key", unique: true
   end
 
   add_foreign_key "follows", "users", column: "followee_id"
